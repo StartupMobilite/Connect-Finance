6e7908c8f9ff22a0fad1dda2a0a14eae54a3678d
Saucisson937 <jordan.rosemond@gmail.com>
2016-01-13 11:14:12 +0100

    Test pour intégration connection fb, google+, linkedin + ajout package newtonsoft.json, xamarin.Auth

---
+++ ConnectFinance/ConnectFinance_1/ConnectFinance_1/ConnectFinance_1/SocialNetwork/GooglePlus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConnectFinance_1.SocialNetwork
+{
+    class GooglePlus
+    {
+
+    }
+}
---
+++ ConnectFinance/ConnectFinance_1/ConnectFinance_1/ConnectFinance_1/SocialNetwork/LinkedIn.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConnectFinance_1.SocialNetwork
+{
+    class LinkedIn
+    {
+    }
+}
